14537a0edc3dc4583f737428c24769f9ee5a3437
Subha <comebackcobain@gmail.com>
2020-11-29 12:11:19 +0000

    Initial commit

---
+++ printing_in_ruby.rb
@@ -0,0 +1,6 @@
+# Printing values is used to check output of any program or as in most of the cases debuging. 
+#Like any other programming language this can also be done in ruby and in different ways. Below are the details.
+
+#Using Puts
+
+puts "Hello World"
